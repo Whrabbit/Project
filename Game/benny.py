05ee708a4a3c4c16cc599df57ec01f81805c2160
Benny Peterson <benny.peterson.1996@gmail.com>
2016-01-25 13:32:34 +0100

    Game_Window()
    Written in classes

--- Game/benny.py
+++ Game/benny.py
@@ -2,26 +2,32 @@ import pygame
 
 pygame.init()
 
-width   =   1000
-height  =   1000
+class window():
+    def __init__(self, screen, bg_color=(255,255,255)):
+
+        self.screen = screen
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+
+        loop = False
+
+        while not loop:
+            self.clock.tick(60)                      #clock.tick is for the smoothness of the frames, in this case 60
+
+            for event in pygame.event.get():         #this function reads the "events" that are made and makes a list, like where are you pointing with your mouse at
+                if event.type == pygame.QUIT:        #QUIT function will recognize when you want to close the window and shuts itself off
+                    loop = True
+                print(event)                         #prints every motion made with a mouse or keyboard
+
+            self.screen.fill(self.bg_color)
+            pygame.display.update()
+
+if __name__ == "__main__":
+    surface = pygame.display.set_mode((640, 480))    #surface width & length
+    pygame.display.set_caption('Survivor')           #title on the window
+    w = window(surface)
+    w.run()
 
 
-surface = pygame.display.set_mode((width,height))
-pygame.display.set_caption('Survivor')
-clock = pygame.time.Clock()
-
-
-start = True
-
-while start:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key ==  pygame.K_ESCAPE):
-            start = False
-        print (event)
-
-    pygame.display.update()
-    clock.tick(60)
-
-pygame.quit()
-quit()
