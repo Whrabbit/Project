ede3a01520ef5eb66a90e1d1af3241e7f5a3354d
Benny Peterson <benny.peterson.1996@gmail.com>
2016-02-04 14:41:11 +0100

    game V1.4

--- Game/benny.py
+++ Game/benny.py
@@ -9,7 +9,112 @@ pygame.display.set_caption('Survivor game group 3')
 
 black = (0,0,0)
 white = (255,255,255)
+red = (240,85,0)import pygame
+import webbrowser
+
+pygame.init()
+width, height = 1000, 800
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Survivor game group 3')
+
+black = (0,0,0)
+white = (255,255,255)
 red = (240,85,0)
+green = (85,240,0)
+brightred = (255,85,0)
+brightgreen = (85,255,0)
+blue = (0,85,240)
+brightblue = (0,85,255)
+
+clock = pygame.time.Clock()
+
+survivor_logo = pygame.image.load('/Users/Benny/Documents/School/Project/Game/final/survivor.png')
+game_board = pygame.image.load('/Users/Benny/Documents/School/Project/Game/final/scaled_boardgame.jpg')
+
+
+
+def survivor(x, y):
+    screen.blit(survivor_logo,(x,y))
+
+x = (width * 0.08)
+y = (height * 0.30)
+
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def button(msg,x,y,w,h,ic,ac,action):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac, (x,y,w,h))
+        if click[0] == 1:
+          action()
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h))
+        smallText = pygame.font.Font("freesansbold.ttf",20)
+        textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ((x+(w/2)),(y+(h/2)))
+        screen.blit(textSurf, textRect)
+
+def start():
+    screen.fill(white)
+    screen.blit(game_board,(a,b))
+
+a = (width * 0)
+b = (height * 0)
+
+
+def open():
+    webbrowser.open_new(r'Manual.pdf')
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+
+def game_intro(start_screen = True):
+    while start_screen:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+
+               start_screen = False
+
+        screen.fill(white)
+        survivor(x, y)
+
+        button("Start Game",(width/5*1),(height/4*3),150,50,green,brightgreen,player_selection)
+        button("Instructions",(width/5*2),(height/4*3),150,50,blue,brightblue,open)
+        button("Quit Game",(width/5*3),(height/4*3),150,50,red,brightred,quitgame)
+
+        largeText = pygame.font.Font('freesansbold.ttf',75)
+        TextSurf, TextRect = text_objects(" ", largeText)
+        TextRect.center = ((width/2),(height/3))
+        screen.blit(TextSurf, TextRect)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+def player_selection():
+    while True:
+        for event in pygame.event.get():
+            i=1
+        start()
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+game_intro()
+pygame.quit()
 green = (85,240,0)
 brightred = (255,85,0)
 brightgreen = (85,255,0)
