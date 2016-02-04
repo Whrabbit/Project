71ce8b9ae08f5d096d11f099ffd286b0a5c561d1
Benny Peterson <benny.peterson.1996@gmail.com>
2016-02-04 02:26:54 +0100

    Game V1.3
    shorter code,

--- Game/benny.py
+++ Game/benny.py
@@ -2,10 +2,10 @@ import pygame
 import webbrowser
 
 pygame.init()
-width, height = 1000, 800
+width, height = 1040, 1040
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Survivor')
+pygame.display.set_caption('Survivor game group 3')
 
 black = (0,0,0)
 white = (255,255,255)
@@ -19,6 +19,8 @@ brightblue = (0,85,255)
 clock = pygame.time.Clock()
 start = True
 survivor_logo = pygame.image.load('/Users/Benny/Desktop/survivor.png')
+game_board = pygame.image.load('/Users/Benny/Desktop/scaled_boardgame.jpg')
+
 #########################################
 def survivor(x, y):
     screen.blit(survivor_logo,(x,y))
@@ -30,13 +32,13 @@ def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-def playbutton(msg,x,y,w,h,ic,ac):
+def button(msg,x,y,w,h,ic,ac,action):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
         pygame.draw.rect(screen, ac, (x,y,w,h))
         if click[0] == 1:
-            screen.fill(white)
+          action()
     else:
         pygame.draw.rect(screen, ic,(x,y,w,h))
         smallText = pygame.font.Font("freesansbold.ttf",20)
@@ -44,41 +46,25 @@ def playbutton(msg,x,y,w,h,ic,ac):
         textRect.center = ((x+(w/2)),(y+(h/2)))
         screen.blit(textSurf, textRect)
 
-def instructionsbutton(msg,x,y,w,h,ic,ac):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(screen, ac, (x,y,w,h))
-        if click[0] == 1:
-            webbrowser.open_new(r'Manual.pdf')
-    else:
-        pygame.draw.rect(screen, ic,(x,y,w,h))
-        smallText = pygame.font.Font("freesansbold.ttf",20)
-        textSurf, textRect = text_objects(msg, smallText)
-        textRect.center = ((x+(w/2)),(y+(h/2)))
-        screen.blit(textSurf, textRect)
-        
-def quitbutton(msg,x,y,w,h,ic,ac):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.rect(screen, ac, (x,y,w,h))
-        if click[0] == 1:
-            pygame.quit(),
-            quit()
-    else:
-        pygame.draw.rect(screen, ic,(x,y,w,h))
-        smallText = pygame.font.Font("freesansbold.ttf",20)
-        textSurf, textRect = text_objects(msg, smallText)
-        textRect.center = ((x+(w/2)),(y+(h/2)))
-        screen.blit(textSurf, textRect)
+def start():
+    screen.blit(game_board,(x,y))
+
+x = (width * 0.07)
+y = (height * 0.30)
+
+def open():
+    webbrowser.open_new(r'Manual.pdf')
+
+def quitgame():
+    pygame.quit()
+    quit()
+
 ################################################################
 while start:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
 
            start = False
-
 
     screen.fill(white)
     survivor(x, y)
@@ -88,9 +74,9 @@ while start:
     TextRect.center = ((width/2),(height/3))
     screen.blit(TextSurf, TextRect)
 
-    playbutton("Start Game",(width/5*1),(height/4*3),150,50,green,brightgreen)
-    instructionsbutton("Instructions",(width/5*2),(height/4*3),150,50,blue,brightblue)
-    quitbutton("Quit Game",(width/5*3),(height/4*3),150,50,red,brightred)
+    button("Start Game",(width/5*1),(height/4*3),150,50,green,brightgreen,start)
+    button("Instructions",(width/5*2),(height/4*3),150,50,blue,brightblue,open)
+    button("Quit Game",(width/5*3),(height/4*3),150,50,red,brightred,quitgame)
 
 
     pygame.display.update()
